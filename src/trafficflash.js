80e6e670afaad2bb097b1fbebb3306df6f68ea8f
lijiaze <lijiaze@outlook.com>
2020-01-08 11:04:18 +0800

    增加组件

---
+++ src/trafficflash.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './App.css';
+
+class Trafficflash extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+     
+    }
+   
+  }
+  componentDidMount(){
+   
+  }
+  render () {
+   
+    return (
+      <div>
+       46363563635635464654
+      </div>
+    ) 
+  }
+
+}
+
+export default Trafficflash;
